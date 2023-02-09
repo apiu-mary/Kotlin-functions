b85a91aff1ce386e145756b5bc85ca27fe1cc459
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-02-09 12:51:08 +0300

    functions

---
+++ Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    greeting()
+    divide(num1 = 12, num2 = 23)
+    add(num1 =15 ,num2 =38,num3 =23, num4 = 42)
+
+}
+fun greeting() {
+    var name = "Apiu"
+    println("HELLO " + name)
+}
+fun divide(num1: Int,num2:Int){
+    var divide= num1%num2
+    return ; divide
+}
+
+fun add(num1: Int,num2: Int,num3:Int,num4: Int){
+    var add = num1 + num2 +num3 + num4
+    return ;  add
+}
+
+
+fun fact(){
+    var fact = "I like laughing a lot"
+    println(fact)
+
+}
+
+
+
+
+
+
+
